be4020c5b26040395bdb78cbbb0750a64ffb7fd2
Milicevic <Milicevic@fidor.de>
2018-09-16 13:56:36 +0200

    configuration

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React                           from 'react';
+import { Navbar, Nav, NavItem, Badge } from 'react-bootstrap';
+
+const Header = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Navbar.Link href="/"> Cryptocurrencies <Badge>100</Badge> </Navbar.Link>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem href="/"> Settings </NavItem>
+    </Nav>
+  </Navbar>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import App                              from './components/App.js';
+import { Provider }                     from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers                         from './reducers';
+import reduxThunk                       from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
